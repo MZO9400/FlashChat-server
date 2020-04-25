8993ff9bf237ff1640d348e3c67ff323f6fe23cf
MZO9400 <hamza.hameed00@yahoo.com>
2020-04-25 06:20:08 +0500

    Add JWT, mongoose model, user signin api, user signup api, bcrypt hashing

---
+++ Routes/users.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../Mongo/Database");
+const User = require("../Mongo/Models/Users");
+
+
+router.post("/register", (req, res) => {
+    User.findOne({email: req.body.email}).then(user => {
+        if (user) {
+            return res.status(400).json({email: "Email already exists"});
+        }
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+            });
+        });
+    });
+});
+
+
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(404).json({ error: "Email not found" });
+        }
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 3600
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({ error: "Password incorrect" });
+            }
+        });
+    });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./Mongo/Database")
+const users = require("./Routes/users");
+const cors = require('cors')
+
+mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database:'+ err)}
+);
+
 const app = express();
 
-const http = require("http").createServer(app);
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
-http.listen(8000, function () {
-    console.log("Server started");
+
+require("./auth/passport")(passport);
+
+app.use("/api/users", users);
+
+app.get('/', function(req, res) {
+    res.send('reached dead endpoint');
+});
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
 });
